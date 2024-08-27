dd1f5fa079c094d0f81c3c79eb11f6f3993bd0f9
apptcion <mojangdungeon@gmail.com>
2024-08-27 22:58:23 +0900

    add Loading

--- src/app/schedule/neis.tsx
+++ src/app/schedule/neis.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Menu from '../menu/menu';
 import styles from '../../css/schedule.module.css';
 import { userAgentFromString } from "next/server";
@@ -26,14 +26,15 @@ function getWeekRange(date) {
 }
 
 export default function Neis() {
-  const [school, setSchool] = useState('단원고등학교');
-  const [grade, setGrade] = useState('1');
-  const [classNumber, setClassNumber] = useState('5');
+  const [school, setSchool] = useState('');
+  const [grade, setGrade] = useState('');
+  const [classNumber, setClassNumber] = useState('');
   const [weekSchedule, setWeekSchedule] = useState([[], [], [], [], [], [], []]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [atptCode, setAtptCode] = useState('J10');
-  const [schoolCode, setSchoolCode] = useState('');
+  let schoolCode = '';
+  let schoolType = '';
 
   const fetchSchool = async () => {
     setLoading(true);
@@ -46,12 +47,14 @@ export default function Neis() {
     try {
       let response = await fetch(neis_school_url);
       let data = await response.json();
-      setSchoolCode(data.schoolInfo[1].row[0].SD_SCHUL_CODE);
+      schoolCode = data.schoolInfo[1].row[0].SD_SCHUL_CODE;
+      schoolType = data.schoolInfo[1].row[0].SCHUL_NM.includes('고등학교') ? 'hi' : data.schoolInfo[1].row[0].SCHUL_NM.includes('중학교') ? 'mi' : 'el';
+      console.log(schoolType);
       setAtptCode(data.schoolInfo[1].row[0].ATPT_OFCDC_SC_CODE);
+      fetchSchedule();
     } catch (e) {
       setError('데이터를 불러오는 중 오류가 발생했습니다.');
-      setSchoolCode('');
-    } finally {
+      schoolCode = '';
       fetchSchedule();
     }
   };
@@ -64,13 +67,24 @@ export default function Neis() {
     let today = new Date();
     let weekRange = getWeekRange(today);
     
-    let neisUrl = `https://open.neis.go.kr/hub/hisTimetable?KEY=${neisKey}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${atptCode}&SD_SCHUL_CODE=${schoolCode}&AY=${today.getFullYear()}&GRADE=${grade}&CLASS_NM=${classNumber}&TI_FROM_YMD=${weekRange.monday}&TI_TO_YMD=${weekRange.friday}`;
+    let neisUrl = `https://open.neis.go.kr/hub/${schoolType}sTimetable?KEY=${neisKey}&Type=json&pIndex=1&pSize=100&ATPT_OFCDC_SC_CODE=${atptCode}&SD_SCHUL_CODE=${schoolCode}&AY=${today.getFullYear()}&GRADE=${grade}&CLASS_NM=${classNumber}&TI_FROM_YMD=${weekRange.monday}&TI_TO_YMD=${weekRange.friday}`;
 
+    console.log(neisUrl);
     try {
       let response = await fetch(neisUrl);
       let data = await response.json();
-      if (data.hisTimetable) {
-        let schedule = data.hisTimetable[1].row;
+      let data_timeTable;
+
+      if (schoolType == 'hi')
+        data_timeTable = data.hisTimetable;
+      else if (schoolType == 'mi')
+        data_timeTable = data.misTimetable;
+      else 
+        data_timeTable = data.elsTimetable;
+
+      if (data_timeTable) {
+        let schedule = data_timeTable[1].row;
+
         let newWeekSchedule = [[], [], [], [], [], [], []];
 
         schedule.forEach(e => {
@@ -91,6 +105,10 @@ export default function Neis() {
   const handleSearch = () => {
     fetchSchool();
   };
+
+  useEffect(() => {
+    fetchSchool();
+  }, []);
 
   return (
     <div>
@@ -119,32 +137,29 @@ export default function Neis() {
       {error && <p>{error}</p>}
 
       {!loading && !error && (
-        <table className={styles.main}>
-          <thead>
-            <tr className={styles.thead}>
-              <th scope='col'></th>
-              <th scope='col'>월</th>
-              <th scope='col'>화</th>
-              <th scope='col'>수</th>
-              <th scope='col'>목</th>
-              <th scope='col'>금</th>
-            </tr>
-          </thead>
-          <tbody>
-            {weekSchedule.map((day, index) => (
-              <tr className={styles.sche_tr} key={index}>
-                <th className={styles.sche_th_row}>{index + 1}교시</th>
+        <div className={styles.table}>
+          <div className={styles.table_row}>
+            <div className={styles.table_header}></div>
+            <div className={styles.table_header}>월</div>
+            <div className={styles.table_header}>화</div>
+            <div className={styles.table_header}>수</div>
+            <div className={styles.table_header}>목</div>
+            <div className={styles.table_header}>금</div>
+          </div>
+          {
+            weekSchedule.map((day, index) => (
+              <div className={styles.table_row} key={index}>
+                <div className={styles.table_row_head}>{index + 1}교시</div>
                 {[...Array(5)].map((_, dayIndex) => (
-                  <th className={styles.sche_th} key={dayIndex}>
+                  <div className={styles.table_cell} key={dayIndex}>
                     {day[dayIndex] || ''}
-                  </th>
+                  </div>
                 ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+              </div>
+            ))
+          }
+        </div>
       )}
-      <Menu />
     </div>
   );
 }
